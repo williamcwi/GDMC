26a6275ddcb574be8d30f9aa667350adf665804e
William <wic2@kent.ac.uk>
2020-11-11 14:12:50 +0000

    GDMC-76 created JSON loader function

--- generateSettlement.py
+++ generateSettlement.py
@@ -4,3 +4,23 @@ import logging.config
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger('Generate Settlement')
 
+inputs = [
+    (
+        ('Settlement Generator', 'title'),
+        
+        ('By William, Selina, Ho Kiu, Rhys', 'label'),
+        ('', 'label'),
+
+    ),
+    (
+        ('Settlement Settings', 'title'),
+        (),
+    )
+]
+
+def perform(level, box, options):
+
+
+def loadFile(filePath):
+    with open(filePath) as f:
+        return json.load(f)
