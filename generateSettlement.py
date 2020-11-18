f8668bdf1438ac3ec98cb7deca2aef9e50b7e8cd
hkl9 <hkl9@kent.ac.uk>
2020-11-18 21:47:15 +0000

    Replaced double-dot file path with os.path.dirname

--- generateSettlement.py
+++ generateSettlement.py
@@ -2,7 +2,7 @@ from pymclevel import TAG_Compound, TAG_Int, TAG_Short, TAG_Byte, TAG_String, TA
 from pymclevel import MCSchematic
 from pymclevel.box import Vector
 
-import os
+import os.path
 
 inputs = [
     (
@@ -22,8 +22,10 @@ def perform(level, box, options):
 
     selection = options['simple house design']
 
-    filename = '{}/../schematics/simple_house/simple_house_{}.schematic'.format(__file__, selection)
+    filename = '{}/schematics/simple_house/simple_house_{}.schematic'.format(os.path.dirname(__file__), selection)
 
     schematic = MCSchematic(shape=(11,6,11), filename=filename)
 
     level.copyBlocksFrom(schematic, schematic.bounds, Vector(box.minx, box.miny+1, box.minz))
+    
+    print(filename)
