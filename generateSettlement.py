54f62ff4eae9470ba400d853a0797e9d17928dc8
William <wic2@kent.ac.uk>
2020-11-19 17:34:49 +0000

    Exception handler

--- generateSettlement.py
+++ generateSettlement.py
@@ -25,12 +25,16 @@ logger = Logger(name)
 
 def perform(level, box, options):
 
-    building_type = 'simple house'
-    randomHouse = random.randint(1, 5)
-    building = '{} {}'.format(building_type, randomHouse)
+    try:
+        building_type = 'simple house'
+        randomHouse = random.randint(1, 5)
+        building = '{} {}'.format(building_type, randomHouse)
 
-    filename = os.path.join(os.path.dirname(__file__), 'schematics', 'simple_house', 'simple_house_{}.schematic'.format(randomHouse))
-    schematic = MCSchematic(shape=(11,6,11), filename=filename)
-    level.copyBlocksFrom(schematic, schematic.bounds, Vector(box.minx, box.miny+1, box.minz))
+        filename = os.path.join(os.path.dirname(__file__), 'schematics', 'simple_house', 'simple_house_{}.schematic'.format(randomHouse))
+        schematic = MCSchematic(shape=(11,6,11), filename=filename)
+        level.copyBlocksFrom(schematic, schematic.bounds, Vector(box.minx, box.miny+1, box.minz))
+        
+        logger.info('Placing {} from {}'.format(building, filename))
     
-    logger.info('Placing {} from {}'.format(building, filename))
+    except exception as e:
+        logger.error(e)
