35b8d980faf3f0a989c9f736fb9bc7271b3c89bc
William <wic2@kent.ac.uk>
2020-11-19 11:38:17 +0000

    Display more info on print

--- generateSettlement.py
+++ generateSettlement.py
@@ -1,8 +1,9 @@
+import os.path
+from datetime import datetime as dt
+
 from pymclevel import TAG_Compound, TAG_Int, TAG_Short, TAG_Byte, TAG_String, TAG_Float, TAG_Double, TAG_List
 from pymclevel import MCSchematic
 from pymclevel.box import Vector
-
-import os.path
 
 inputs = [
     (
@@ -18,14 +19,16 @@ inputs = [
     )
 ]
 
+name = 'generateSettlement'
+
 def perform(level, box, options):
 
+    building_type = 'simple house'
     selection = options['simple house design']
+    building = '{} {}'.format(building_type, selection)
 
-    filename = '{}/schematics/simple_house/simple_house_{}.schematic'.format(os.path.dirname(__file__), selection)
-
+    filename = '{}\schematics\simple_house\simple_house_{}.schematic'.format(os.path.dirname(__file__), selection)
     schematic = MCSchematic(shape=(11,6,11), filename=filename)
-
     level.copyBlocksFrom(schematic, schematic.bounds, Vector(box.minx, box.miny+1, box.minz))
     
-    print(filename)
+    print('{} {} INFO: Placing {} from {}'.format(dt.now(), name, building, filename))
