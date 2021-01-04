f14a60d89b6b47c07a28c0070f20188de8fa8b93
William <williamcwi.chu@hotmail.com>
2021-01-04 17:57:29 +0000

    GDMC-102 Sobel Operator to find edges (vertical)

--- heightmap/processHeightmap.py
+++ heightmap/processHeightmap.py
@@ -19,3 +19,18 @@ for height in range(50, 75): # for height in dataset
                 new_row.append(0)
         new_heightmap.append(new_row)
     # print(new_heightmap)
+
+    # # Sobel Operator
+    # edge_map = []
+    # for i in range(len(new_heightmap)): # row
+    #     if i == 0:
+    #         edge_map.append(np.zeros(256).astype(int).tolist())
+    #     else:
+    #         edge_row = []
+    #         for j, k in zip(new_heightmap[i], new_heightmap[i-1]): # compare current row and previous row
+    #             if j == k:
+    #                 edge_row.append(0)
+    #             else:
+    #                 edge_row.append(1)
+    #         edge_map.append(edge_row)
+    # print(edge_map)
