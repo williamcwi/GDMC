53729f80527e5f43848d39d2401b780a9e7d3198
William <williamcwi.chu@hotmail.com>
2021-01-13 14:11:40 +0000

    GDMC-119 flow for generateWalls

--- generateWalls.py
+++ generateWalls.py
@@ -6,3 +6,106 @@ from pymclevel.box import Vector
 name = 'generateWalls'
 logger = Logger(name)
 
+def calc_wall_sections(box):
+    try:
+        # TODO: calculate and return:
+        # - x_left
+        # - x_right
+        # - z_left
+        # - z_right
+        # - gate
+        # 8 Blocks each side for the corners
+        # remaining blocks between 6-9 inclusive for gate
+        pass
+    except Exception as e:
+        logger.error(e)
+
+def place_wall_sections(level, box):
+    try:
+        # TODO: place wall sections
+            # +z:
+            # |-********-|
+            # |          |
+            # |          |
+            # |          |
+            # |          |
+            # |__________|
+
+            # -z:
+            # |----------|
+            # |          |
+            # |          |
+            # |          |
+            # |          |
+            # |_********_|
+
+            # +x:
+            # |----------|
+            # |          |
+            # |          *
+            # |          *
+            # |          |
+            # |__________|
+
+            # -x:
+            # |----------|
+            # |          |
+            # *          |
+            # *          |
+            # |          |
+            # |__________|
+        pass
+    except Exception as e:
+        logger.error(e)
+
+def place_wall_corners(level, box):
+    try:
+        # TODO: place wall corners
+            # -x +z:
+            # **---------|
+            # *          |
+            # |          |
+            # |          |
+            # |          |
+            # |__________|
+
+            # +x +z:
+            # |---------**
+            # |          *
+            # |          |
+            # |          |
+            # |          |
+            # |__________|
+
+            # -x -z:
+            # |----------|
+            # |          |
+            # |          |
+            # |          |
+            # *          |
+            # **_________|
+
+            # +x -z:
+            # |----------|
+            # |          |
+            # |          |
+            # |          |
+            # |          *
+            # |_________**
+        pass
+    except Exception as e:
+        logger.error(e)
+
+def place_gates(level, box):
+    try:
+        # TODO: place gates based on gate size
+        pass
+    except Exception as e:
+        logger.error(e)
+
+def place_walls(level, box):
+    try:
+        # TODO: place walls together
+        pass
+    except Exception as e:
+        logger.error(e)
