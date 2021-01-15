f665cb0f6af566f8bcd096f07560f6f73a8af1e0
selinasoo <selina.soo@hotmail.co.uk>
2021-01-15 16:51:58 +0000

    GDMC-117 Added bounding box

--- deforestation.py
+++ deforestation.py
@@ -1,6 +1,8 @@
 from logger import Logger
 
 from pymclevel import alphaMaterials as am
+from pymclevel.box import Vector
+from pymclevel.box import BoundingBox
 
 name = 'deforestation'
 logger = Logger(name)
@@ -8,73 +10,76 @@ logger = Logger(name)
 tree = [
     #Leaves
     am.Leaves,
-    am.SpruceLeaves,
+    am.PineLeaves,
     am.BirchLeaves,
     am.JungleLeaves,
     am.AcaciaLeaves,
     am.DarkOakLeaves,
     #Logs
     am.Wood,
-    am.SpruceWood,
+    am.PineWood, #Spruce
     am.BirchWood,
     am.JungleWood,
-    am.AcaciaWood,
-    am.DarkOakWood
+    am.Wood2, #Acacia and dark oak wood
 ]
 
 logs = [
     am.Wood,
-    am.SpruceWood,
+    am.PineWood,
     am.BirchWood,
     am.JungleWood,
-    am.AcaciaWood,
-    am.DarkOakWood
+    am.Wood2,
 ]
 
 treeID = [t.ID for t in tree]
 logsID = [l.ID for l in logs]
-print(treeID)
 
+# Creates a smaller box within selection to get rid of trees 
 def setBoundingBox(box):
     # TODO: smaller bounding box
     try:
-        pass
+        # Starts 3 blocks inside selection 
+        origin = Vector((box.minx+3), box.miny, (box.minz+3))
+        # Makes box selection smaller
+        size =  Vector((box.maxx-box.minx-6), (box.maxy - box.miny), (box.maxz-box.minz-6))
+
+        return BoundingBox(origin, size)
+
     except Exception as e:
         logger.error(e)
 
 def removeTrees(level, box):
     try:
-        pass
-        # for (chunk, slices, point) in level.getChunkSlices(box):
-        #     blocks = chunk.Blocks[slices]
+        for (chunk, slices, point) in level.getChunkSlices(box):
+            blocks = chunk.Blocks[slices]
         #     Change blocks to air
-        #     blocks[blocks == 2] = 0
-
-            # chunk.chunkChanged()
+            for t in treeID:
+                blocks[blocks == t] = 0
+                #logger.info("Going through loop")
+        # chunk.chunkChanged()
 
     except Exception as e:
         logger.error(e)
 
 def removeLogs(level, box):
     try:
-        pass
-        # for (chunk, slices, point) in level.getChunkSlices(box):
-        #     blocks = chunk.Blocks[slices]
-        #     blocks[blocks == 2] = 41
-
-            # chunk.chunkChanged()
+        
+        for (chunk, slices, point) in level.getChunkSlices(box):
+            blocks = chunk.Blocks[slices]
+        #     Change blocks to air
+            for l in logsID:
+                blocks[blocks == l] = 0
+        # chunk.chunkChanged()
 
     except Exception as e:
         logger.error(e)
 
 def deforestation(level, box):
     try:
-        pass
-        # big_box = box
-        # small_box = setBoundingBox(box)
+        big_box = box
+        small_box = setBoundingBox(box)
 
-        # removeTrees(level, small_box)
-        # removeLogs(level, big_box)
-
+        removeTrees(level, small_box)
+        removeLogs(level, big_box)
     except Exception as e:
         logger.error(e)
