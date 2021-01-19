e95f81a40fc93a632d63508c5b874f61310d1631
William <williamcwi.chu@hotmail.com>
2021-01-19 15:45:13 +0000

    GDMC-121 Moved editTerrain to terrain.py

--- generateSettlement.py
+++ generateSettlement.py
@@ -7,6 +7,7 @@ import common
 import heightmap
 import generateStructure
 import deforestation
+import terrains
 
 inputs = [
     (
@@ -36,43 +37,18 @@ def perform(level, box, options):
         # Create Height Map
         heightMap = heightmap.heightMap(level, box)
 
+        # TODO: move to separate file
         # Read height map difference file to string array (replace with correct file path)
         with open('C:/Users/Rhys/Documents/coursework/Year 3/CO600 - Project/mcedit/stock-filters/HMD-145407.txt') as heightMapDiff:
             lines = [line.split() for line in heightMapDiff]
-
         # Convert string array to int
         heightMapDiffInt = [list(map(int,i)) for i in lines]
 
         # Edit terrain based on height map
-        editTerrain(level, box, heightMap, heightMapDiffInt)
+        terrains.editTerrain(level, box, heightMap, heightMapDiffInt)
 
         # Generate simple house
         # generateStructure.generateSimpleHouse(level, box)
 
-    except Exception as e:
-        logger.error(e)
-
-def editTerrain (level, box, oldHeightMap, heightMapDiff):
-    try:
-        zpos = 0
-        for z in xrange(box.minz, box.maxz):
-            xpos = 0
-            for x in xrange(box.minx, box.maxx):
-                ydiff = heightMapDiff[zpos][xpos]
-                if ydiff < 0:
-                    oldy = oldHeightMap[zpos][xpos]
-                    newy = oldy + ydiff
-                    while oldy > newy:
-                        level.setBlockAt(x, oldy, z, 0)
-                        oldy -= 1
-                if ydiff > 0:
-                    oldy = oldHeightMap[zpos][xpos]
-                    newy = oldy + ydiff
-                    block = level.blockAt(x, oldy, z)
-                    while oldy < newy:
-                        oldy += 1
-                        level.setBlockAt(x, oldy, z, block)
-                xpos += 1
-            zpos += 1
     except Exception as e:
         logger.error(e)
