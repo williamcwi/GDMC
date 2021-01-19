c79a97df2f500eae0a5fbbf6a13f6827dba48037
rad33@kent.ac.uk <rad33@kent.ac.uk>
2021-01-19 15:10:58 +0000

    GDMC-121 - Edit terrain based on new heightmap

--- generateSettlement.py
+++ generateSettlement.py
@@ -2,6 +2,7 @@ from logger import Logger
 
 from pymclevel import TAG_Compound, TAG_Int, TAG_Short, TAG_Byte, TAG_String, TAG_Float, TAG_Double, TAG_List
 
+import os
 import common
 import heightmap
 import generateStructure
@@ -30,13 +31,48 @@ def perform(level, box, options):
         box = common.expandBoundingBox(box)
 
         # Deforestation
-        deforestation.deforestation(level, box)
+        deforestation.removeTrees(level, box)
 
         # Create Height Map
-        # heightmap.heightMap(level, box)
+        heightMap = heightmap.heightMap(level, box)
+
+        # Read height map difference file to string array (replace with correct file path)
+        with open('C:/Users/Rhys/Documents/coursework/Year 3/CO600 - Project/mcedit/stock-filters/HMD-145407.txt') as heightMapDiff:
+            lines = [line.split() for line in heightMapDiff]
+
+        # Convert string array to int
+        heightMapDiffInt = [list(map(int,i)) for i in lines]
+
+        # Edit terrain based on height map
+        editTerrain(level, box, heightMap, heightMapDiffInt)
 
         # Generate simple house
         # generateStructure.generateSimpleHouse(level, box)
 
+    except Exception as e:
+        logger.error(e)
+
+def editTerrain (level, box, oldHeightMap, heightMapDiff):
+    try:
+        zpos = 0
+        for z in xrange(box.minz, box.maxz):
+            xpos = 0
+            for x in xrange(box.minx, box.maxx):
+                ydiff = heightMapDiff[zpos][xpos]
+                if ydiff < 0:
+                    oldy = oldHeightMap[zpos][xpos]
+                    newy = oldy + ydiff
+                    while oldy > newy:
+                        level.setBlockAt(x, oldy, z, 0)
+                        oldy -= 1
+                if ydiff > 0:
+                    oldy = oldHeightMap[zpos][xpos]
+                    newy = oldy + ydiff
+                    block = level.blockAt(x, oldy, z)
+                    while oldy < newy:
+                        oldy += 1
+                        level.setBlockAt(x, oldy, z, block)
+                xpos += 1
+            zpos += 1
     except Exception as e:
         logger.error(e)
