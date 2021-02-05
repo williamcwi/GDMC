9238f200380b7cc748bb3ed8507e62df82d99f86
hkl9 <hkl9@kent.ac.uk>
2021-02-05 03:23:07 +0000

    Removing Legacy Code

--- terrains.py
+++ terrains.py
@@ -146,35 +146,7 @@ def editTerrainFF(level, box, alterDict, alterHeightdict):
     except Exception as e:
         logger.error(e)
 
-def editTerrain (level, box, oldHeightMap, heightMapDiff):
-    try:
-        zpos = 0
-        for z in xrange(box.minz, box.maxz):
-            xpos = 0
-            for x in xrange(box.minx, box.maxx):
-                ydiff = heightMapDiff[zpos][xpos]
-                if ydiff < 0:
-                    oldy = oldHeightMap[zpos][xpos]
-                    newy = oldy + ydiff
-                    block = level.blockAt(x, oldy, z)
-                    while oldy > newy:
-                        level.setBlockAt(x, oldy, z, 0)
-                        oldy -= 1
-                    level.setBlockAt(x, oldy, z, block)
-                if ydiff > 0:
-                    oldy = oldHeightMap[zpos][xpos]
-                    newy = oldy + ydiff
-                    block = level.blockAt(x, oldy, z)
-                    while oldy == newy:
-                        oldy += 1
-                        level.setBlockAt(x, oldy, z, block)
-                xpos += 1
-            zpos += 1
-    except Exception as e:
-        logger.error(e)
-
 def pavingGate(level, box, heightMap):
-    alterDict = dict()
 
     def getGatePostion(boxwidth):
         left = 0
