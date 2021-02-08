b46e0970b395ce2eccb9099981b39fb994699c27
rad33@kent.ac.uk <rad33@kent.ac.uk>
2021-02-08 12:33:12 +0000

    GDMC-152 Biome selection to include all biomes

--- biomes.py
+++ biomes.py
@@ -1,17 +1,25 @@
 from logger import Logger
+import math
+import collections
+import numpy as np
 
 logger = Logger('biomes')
 
 def selectBiome(level, box, heightMap):
     try:
-        if island(level, box, heightMap):
-            return "island"
-        else:
-            return "default"
+        biomeArray = np.array([])
+        for z in range(0, box.length, 16):
+            for x in range(0, box.width, 16):
+                chunk = level.getChunk((box.minx + x)/16, (box.minz + z)/16)
+                array = np.array(chunk.root_tag["Level"]["Biomes"].value)
+                biomeArray = np.concatenate((biomeArray, array))
+        biomeCounter = collections.Counter(biomeArray)
+        biome = max(biomeArray, key = biomeCounter.get)
+        return biome, isIsland(level, box, heightMap)
     except Exception as e:
         logger.error(e)
 
-def island(level, box, heightMap):
+def isIsland(level, box, heightMap):
     try:
         waterBorderBlocks = 0
         for z in range(box.length):
