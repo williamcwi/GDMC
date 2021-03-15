f3f4a3c4dcfcb2860f57055a0077a886cab5e2b6
William <williamcwi.chu@hotmail.com>
2021-03-15 17:43:19 +0000

    GDMC-192 completed A* algorithm

--- path.py
+++ path.py
@@ -186,24 +186,34 @@ class Node():
         self.g = 0
         self.h = 0
         self.f = 0
+    
+    def __eq__(self, other):
+        return self.position == other.position
 
 def aStar(mapArr, start, end):
-    # input: start pos and end pos ([x1, z1], [x2, z2])
-    
+    # input: mapArr, start pos and end pos (map, (x1, z1), (x2, z2))
+
     # initialise open and close list
     open_list = [] # nodes visited but not expanded
     close_list = [] # nodes visited and expanded
-    
+
+    # Create start and end node
     start_node = Node(None, start)
-    start_node.g = start_node.h = start_node.f = 0
+    start_node.g = 0
+    start_node.h = euclid(start, end)
+    start_node.f = start_node.g + start_node.h
+
     end_node = Node(None, end)
-    end_node.g = end_node.h = end_node.f = 0
-    
+    end_node.g = 999999
+    end_node.h = 0
+    end_node.f = end_node.g + end_node.h
+
     # add start node to open list
     open_list.append(start_node)
-    
+
+    # Loop until you find the end
     while len(open_list) > 0:
-        
+
         # get current node (lowest f)
         current_node = open_list[0]
         current_index = 0
@@ -211,10 +221,9 @@ def aStar(mapArr, start, end):
             if item.f < current_node.f:
                 current_node = item
                 current_index = index
-        
         open_list.pop(current_index) # remove current node from open list
         close_list.append(current_node) # add current node to close list
-        
+
         if current_node == end_node:
             path = []
             current = current_node
@@ -223,28 +232,42 @@ def aStar(mapArr, start, end):
                 current = current.parent
             reversed_path = path[::-1]
             return reversed_path
-            
+
         # generate children
         children = []
-        for new_position in [[-1, 1], [0, 1], [1, 1], [-1, 0], [1, 0], [-1, -1], [-1, 0], [-1, 1]]:
-            if new_position in [[-1, 1], [1, 1], [-1, -1], [1, -1]]:
+
+        adjacent = [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+        orthogonal = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+
+        # all adjacent including diagonal
+        for new_position in adjacent:
+
+            if new_position in [(-1, 1), (1, 1), (-1, -1), (1, -1)]:
                 dist = 1.4
             else:
                 dist = 1.0
-            node_position = [current_node.position[0] + new_position[0], current_node.position[1] + new_position[1]]
-            if node_position[0] > (len(mapArr) - 1) or node_position[0] < 0 or node_position[1] > (len(mapArr[len(mapArr) - 1]) - 1) or node_position[1] < 0:
+        
+        # # orthogonally adjacent
+        # for new_position in orthogonal:
+        #     dist = 1.0
+
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            if node_position[0] > (len(mapArr) - 1) or node_position[0] < 0 or node_position[1] > (len(mapArr[len(mapArr)-1]) -1) or node_position[1] < 0:
                 # position out of range
                 continue
+
             if mapArr[node_position[0]][node_position[1]] != 0:
                 # non-walkable terrain
                 continue
+
             new_node = Node(current_node, node_position)
             # g, h, f values
             new_node.g = current_node.g + dist
             new_node.h = euclid(node_position, end_node.position)
             new_node.f = new_node.g + new_node.h
             children.append(new_node)
-            
+
         for child in children:
             for closed_child in close_list:
                 if child == closed_child:
@@ -252,14 +275,20 @@ def aStar(mapArr, start, end):
                     continue
             for open_node in open_list:
                 if child == open_node and child.g > open_node.g:
+                    # greater g distance from start
                     continue
-            
             open_list.append(child)
+
+def newMap(mapArr, start, end):
+    mapArr[start[0]][start[1]] = 0
+    mapArr[end[0]][end[1]] = 0
+    # print(mapArr)
+    return mapArr
 
 
 # =============================================================================================
 def run(mapArr):
-    try:
+    # try:
 
         startingPositions = getStartingPosition(mapArr)
 
@@ -279,16 +308,20 @@ def run(mapArr):
         # print(links)
 
         aStarStarting = getAStarStarting(pairsList, links)
-        # print('aStarStarting')
-        # print(aStarStarting)
+        print('aStarStarting: ')
+        print(aStarStarting)
         
+        print('paths: ')
         for pair in aStarStarting:
-            path = aStar(mapArr, pair[0], pair[1])
-            print('path: ')
+            new_map = newMap(mapArr, pair[0], pair[1])
+            start = (pair[0][0], pair[0][1])
+            end = (pair[1][0], pair[1][1])
+            # print(start, end)
+            path = aStar(new_map, start, end)
             print(path)
 
-    except Exception as e:
-        logger.error(e)
+    # except Exception as e:
+    #     logger.error(e)
 
 
 # testing
